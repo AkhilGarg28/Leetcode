710a0d57a86455f12a126defc1d6682065270b9c
Akhil Garg <akhilgarg064@gmail.com>
2026-09-01 11:03:22 +0530

    Time: 1 ms (87.96%), Space: 45.2 MB (95.48%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,26 +1,24 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        boolean[] used = new boolean[nums.length];
-        backtrack(nums, new ArrayList<>(), used, result);
-        return result;
-    }
-
-    private void backtrack(int[] nums, List<Integer> current, boolean[] used, List<List<Integer>> result) {
-        if (current.size() == nums.length) {
-            result.add(new ArrayList<>(current));
+class Solution {
+    public static void solve(int[] arr, int idx,List<Integer> curr, boolean[] use,List<List<Integer>> res){
+        if(idx == arr.length){
+            res.add(new ArrayList<>(curr));
             return;
         }
-        for (int i = 0; i < nums.length; i++) {
-            if (used[i]) continue;
-            used[i] = true;
-            current.add(nums[i]);
-            backtrack(nums, current, used, result);
-            current.remove(current.size() - 1);
-            used[i] = false;
+        for(int i=0;i<arr.length;i++){
+            if(!use[i]){
+                curr.add(arr[i]);
+                use[i] = true;
+                solve(arr,idx+1,curr,use,res);
+                curr.remove(curr.size()-1);
+                use[i] = false;
+            }
         }
+        
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res= new ArrayList<>();
+        int n = nums.length;
+        solve(nums,0,new ArrayList<>(),new boolean[n],res);
+        return res;
     }
 }
